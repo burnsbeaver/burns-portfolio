a405f656aee0605d9ab251ff48538855cc263c17
burnsbeaver <burnsbeaver@gmail.com>
2017-08-06 16:39:24 -0400

    Added navbar and foundation for the skills tab.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,11 +4,11 @@ class NavBar extends Component {
   render(){
     return(
       <div className="navbar">
-        <h2>Burns Beaver</h2>
-        <a href="#">About Me</a>
-        <a href="#">Skills</a>
-        <a href="#">Projects</a>
-        <a href="#">Contact</a>
+        <h2>{'Burns Beaver {Web Developer}'}</h2>
+        <button className="btn"><a href="#">About Me</a></button>
+        <button className="btn"><a href="#">Skills</a></button>
+        <button className="btn"><a href="#">Projects</a></button>
+        <button className="btn"><a href="#">Contact</a></button>
       </div>
     )
   }
