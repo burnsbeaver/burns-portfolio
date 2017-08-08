52670473c9be140855e856989f70c3d97765e90d
burnsbeaver <burnsbeaver@gmail.com>
2017-08-08 14:01:48 -0400

    Implementing random typer

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
 
 class Jumbotron extends Component {
+  constructor(){
+    super()
+    this.state = {
+      words: ''
+    }
+  }
+
+
+typeIntro = function (input) {
+    function random() {
+      return (4 * (Math.random() * 200))
+    }
+
+    var name = input.split('');
+    var timeoutEffect = 500;
+
+    for (var i = 0; i < name.length; i++) {
+      this.logLetter(name[i], timeoutEffect)
+      timeoutEffect += random()
+    }
+}
+
+logLetter = function (letter, time){
+  var string = ""
+  console.log(string)
+  setTimeout(function(){
+    this.string += letter
+    this.setState= {words: this.string}
+  }, time);
+}
+
   render(){
     return (
       <div className="jumbotron-image">
-        <span>Burns Beaver, <br/> Full Stack Web Developer</span>
+        <span>{this.state.words}</span>
       </div>
     )
+    this.typeIntro('TEST')
   }
 }
 
